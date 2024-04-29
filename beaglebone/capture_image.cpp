b856eff390069715910c354fe4b9794065c2d844
SuperMusey <rishavde.1729@gmail.com>
2024-04-29 19:15:58 -0400

    Revert "Updated code to make it compatible with OpenCV4"
    This reverts commit de0c57df8e1749a37caf448f8f8ef8fb5ff963fb.

--- beaglebone/capture_image.cpp
+++ beaglebone/capture_image.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-
+#include<iostream>
+#include<opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
 
@@ -8,25 +7,18 @@ int main()
 {   
     // Set to /dev/video0
     VideoCapture capture(0);
+    capture.set(CAP_PROP_FOURCC,0x47504A4D);
+    capture.set(CV_CAP_PROP_FRAME_WIDTH,480);
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT,480);
     if(!capture.isOpened()){
-        cout << "Failed to connect to the camera." << endl;
-        return -1;
+	    cout << "Failed to connect to the camera." << endl;
     }
-
-    // Set the codec for video writing
-    int fourcc = VideoWriter::fourcc('M', 'J', 'P', 'G');
-    capture.set(CAP_PROP_FOURCC, fourcc);
-
-    // Set the frame width and height
-    capture.set(CAP_PROP_FRAME_WIDTH, 480);
-    capture.set(CAP_PROP_FRAME_HEIGHT, 480);
-
     // Capturing a single frame/image
     Mat frame;
     capture >> frame;
     if(frame.empty()){
-        cout << "Failed to capture an image" << endl;
-        return -1;
+	cout << "Failed to capture an image" << endl;
+	return -1;
     }
     imwrite("image.png", frame);
     return 0;
